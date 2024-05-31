fdb7f8d6dd84c8594bd7336a87934e11de0ad5ca
Saba Madadi <sabamadadi9@gmail.com>
2024-05-31 12:21:25 +0330

    Add files via upload

---
+++ HashCollisionChecker.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HashCollisionChecker {
+    public static <T> int countOfUniqueHashCodes(HashSet<T> set) {
+        // TODO: Implement
+        return 0;
+    }
+
+    public static <K, V> int countOfUniqueHashCodes(HashMap<K, V> map) {
+        // TODO: Implement
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("c#c#c#c#c#c#bBc#c#c#c#bBc#");
+        set.add("abcd");
+        set.add("c#c#c#c#c#c#bBc#c#c#c#c#aa");
+        set.add("1234");
+        set.add("c#c#c#c#c#c#bBc#c#c#c#c#bB");
+        System.out.println(countOfUniqueHashCodes(set)); // 3
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("c#c#c#c#c#c#c#aaaaaaaabBbB", 14);
+        map.put("c#c#c#c#c#c#c#aaaaaaaac#c#", 12);
+        map.put("c#c#c#c#c#c#c#aaaaaaaac#cc", 16);
+        System.out.println(countOfUniqueHashCodes(map)); // 2
+    }
+}
